29021d2cda3ba29a2200344c261ab0cede06ddce
Rohan Das <hello.rd24@gmail.com>
2021-09-04 12:15:35 +0530

    Added basic customer route

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"fullName"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipCode"`
+}
+
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!!")
 }
 
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Rohan Das", City: "Karimganj", Zipcode: "788710"},
+		{Name: "Shalini Pathak", City: "Kolkata", Zipcode: "700084"},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+}
+
 func main() {
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
